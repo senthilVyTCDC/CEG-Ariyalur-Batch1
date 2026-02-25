5d6af36eefebee1d49799beda7c76c3dc190de4a
rajeshkumarsn <rajeshkumarsn64@gmail.com>
2026-02-25 20:30:29 +0530

    transaction table connected

--- CreditCardFraud/App.py
+++ CreditCardFraud/App.py
@@ -1,16 +1,30 @@
 from flask import Flask, render_template
+import mysql.connector
 
 app = Flask(__name__)
 
 
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="rajesh",
+    database="creditcardfraud",
+    port=3306
+)
+
+cursor = db.cursor(dictionary=True)
+
 
 @app.route('/')
 def Home():
-    return render_template('card.html')
+    return render_template('index.html')
 
 @app.route('/transaction')
 def transaction():
-    return render_template('transaction.html')
+    cursor.execute("SELECT * FROM transaction_")
+    data = cursor.fetchall()
+
+    return render_template("transaction.html", records=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
