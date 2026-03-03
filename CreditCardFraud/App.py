f051c641c05485019d28efe5d2e2743b2ffd7929
SenthilVyTCDC <senthil@vytcdc.com>
2026-03-03 10:24:03 +0530

    CRUD Operations

--- CreditCardFraud/App.py
+++ CreditCardFraud/App.py
@@ -1,11 +1,35 @@
 from flask import Flask, render_template
+from mysql.connector import *
 
 app = Flask(__name__)
 
+dbconnection = connect(
+    host='localhost',
+    user='root',
+    password='root@123',
+    port='3306',
+    database='fsdintern',
+)
+cursor = dbconnection.cursor(dictionary=True)
+
+@app.route('/Home')
+def Display_Student():
+    cursor.execute("SELECT * FROM student")
+    users = cursor.fetchall()   
+    return render_template('indexdb.html', users=users)
+    # return jsonify(users)
 
 
 @app.route('/')
 def Home():
+    return render_template('index.html')
+
+@app.route('/user')
+def User():
+    return render_template('user.html')
+
+@app.route('/card')
+def Card():
     return render_template('card.html')
 
 @app.route('/transaction')
