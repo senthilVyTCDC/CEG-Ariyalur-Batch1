bd5146d044166c6aab8fe69c86ab7b549a45e863
CharuPriya <priyacharu663@gmail.com>
2026-03-04 10:37:07 +0530

    Updated CRUD operations and added CSS & JS static files

--- CreditCardFraud/App.py
+++ CreditCardFraud/App.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import mysql.connector
 
 app = Flask(__name__)
 
@@ -8,9 +9,75 @@ app = Flask(__name__)
 def Home():
     return render_template('card.html')
 
+
 @app.route('/transaction')
 def transaction():
     return render_template('transaction.html')
+
+@app.route('/card')
+def Display_Card():
+    cursor.execute("SELECT * FROM card")
+    card = cursor.fetchall()
+    return render_template('card.html', card=card)
+
+
+# CREATE 
+@app.route('/add', methods=['GET', 'POST'])
+def add_card():
+    if request.method == 'POST':
+        user_id = request.form['user_id']
+        card_number = request.form['card_number']
+        card_type = request.form['card_type']
+        expiry_date = request.form['expiry_date']
+
+        sql = """
+        INSERT INTO card (user_id, card_number, card_type, expiry_date)
+        VALUES (%s, %s, %s, %s)
+        """
+
+        values = (user_id, card_number, card_type, expiry_date)
+
+        cursor.execute(sql, values)
+        dbconnection.commit()
+
+        return redirect(url_for('Display_Card'))
+
+    return render_template('add_card.html')
+
+# EDIT CARD 
+@app.route('/edit/<int:card_id>', methods=['GET', 'POST'])
+def edit_card(card_id):
+
+    if request.method == 'POST':
+        user_id = request.form['user_id']
+        card_number = request.form['card_number']
+        card_type = request.form['card_type']
+        expiry_date = request.form['expiry_date']
+        sql = """
+        UPDATE card
+        SET user_id=%s, card_number=%s, card_type=%s, expiry_date=%s
+        WHERE card_id=%s
+        """
+        values = (user_id, card_number, card_type, expiry_date, card_id)
+        cursor.execute(sql, values)
+        dbconnection.commit()
+        return redirect(url_for('Display_Card'))
+
+    # GET request 
+    cursor.execute("SELECT * FROM card WHERE card_id=%s", (card_id,))
+    card = cursor.fetchone()
+    return render_template('edit_card.html', card=card)
+
+# DELETE
+@app.route('/delete/<int:card_id>', methods=['POST'])
+def delete_card(card_id):
+
+    sql = "DELETE FROM card WHERE card_id=%s"
+    cursor.execute(sql, (card_id,))
+    dbconnection.commit()
+
+    return redirect(url_for('Display_Card'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
