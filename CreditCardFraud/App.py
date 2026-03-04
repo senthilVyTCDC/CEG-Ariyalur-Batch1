20fce2e5c26b03f823687f126c7a31f1c212b18e
RevanthAjay <revanthajaysk@gmail.com>
2026-03-05 00:40:34 +0530

    Updated user

--- CreditCardFraud/App.py
+++ CreditCardFraud/App.py
@@ -4,21 +4,15 @@ import math
 
 app = Flask(__name__)
 
-<<<<<<< HEAD
-db = mysql.connector.connect(
+dbconnection = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="revanth",
-    database="CreditCardFraud"
-=======
-def get_db():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="rajesh",
-        database="creditcardfraud",
-        port=3306
-    )
+    password="rajesh",
+    database="creditcardfraud",
+    port=3306
+)
+
+cursor = dbconnection.cursor(dictionary=True)
 
 @app.route('/')
 def home():
@@ -26,12 +20,10 @@ def home():
 
 @app.route('/transaction')
 def transaction():
+
     page = int(request.args.get('page', 1))
     per_page = 100
     offset = (page - 1) * per_page
-
-    db = get_db()
-    cursor = db.cursor(dictionary=True)
 
     cursor.execute("SELECT COUNT(*) AS count FROM transaction_")
     total = cursor.fetchone()['count']
@@ -44,9 +36,6 @@ def transaction():
 
     records = cursor.fetchall()
 
-    cursor.close()
-    db.close()
-
     total_pages = math.ceil(total / per_page)
 
     return render_template(
@@ -56,38 +45,35 @@ def transaction():
         total_pages=total_pages
     )
 
-@app.route('/transaction_add_form')
-def transaction_add_form():
+@app.route('/transaction_add', methods=['GET', 'POST'])
+def transaction_add():
+
+    if request.method == 'POST':
+
+        cursor.execute("""
+            INSERT INTO transaction_
+            (user_id, card_id, merchant_id, amount, transaction_time, status)
+            VALUES (%s,%s,%s,%s,NOW(),%s)
+        """, (
+            request.form['user_id'],
+            request.form['card_id'],
+            request.form['merchant_id'],
+            request.form['amount'],
+            request.form['status']
+        ))
+
+        dbconnection.commit()
+
+        return redirect(url_for('transaction'))
+
     return render_template("transaction_add.html")
 
-@app.route('/transaction_add', methods=['POST'])
-def add_transaction():
-    db = get_db()
-    cursor = db.cursor()
-
-    cursor.execute("""
-        INSERT INTO transaction_
-        (user_id, card_id, merchant_id, amount, transaction_time, status)
-        VALUES (%s, %s, %s, %s, NOW(), %s)
-    """, (
-        request.form['user_id'],
-        request.form['card_id'],
-        request.form['merchant_id'],
-        request.form['amount'],
-        request.form['status']
-    ))
-
-    db.commit()
-    cursor.close()
-    db.close()
-
-    return redirect(url_for('transaction'))
+@app.route('/transaction_update', methods=['GET'])
+def transaction_update():
+    return render_template("transaction_update.html")
 
 @app.route('/transaction_update/<int:id>', methods=['POST'])
 def update_transaction(id):
-
-    db = get_db()
-    cursor = db.cursor()
 
     cursor.execute("""
         UPDATE transaction_
@@ -99,62 +85,26 @@ def update_transaction(id):
         id
     ))
 
-    db.commit()
-    cursor.close()
-    db.close()
+    dbconnection.commit()
 
     return redirect(url_for('transaction'))
+
+@app.route('/transaction_delete', methods=['GET'])
+def transaction_delete():
+    return render_template("transaction_delete.html")
 
 @app.route('/transaction_delete/<int:id>', methods=['POST'])
 def delete_transaction(id):
 
-    db = get_db()
-    cursor = db.cursor()
+    cursor.execute(
+        "DELETE FROM transaction_ WHERE transaction_id=%s",
+        (id,)
+    )
 
-    cursor.execute("DELETE FROM transaction_ WHERE transaction_id=%s", (id,))
-
-    db.commit()
-    cursor.close()
-    db.close()
+    dbconnection.commit()
 
     return redirect(url_for('transaction'))
 
-dbconnection = mysql.connector.connect(
-    host='localhost',
-    user='root',
-    password='1234',
-    port=3306,
-    database='CreditCardFraud'
->>>>>>> 5425bdd466abb6653976fcfbfe443e03648138a1
-)
-
-cursor = db.cursor(dictionary=True)
-
-<<<<<<< HEAD
-@app.route('/')
-def Home():
-    return render_template('index.html')
-
-@app.route('/transaction')
-def transaction():
-    cursor.execute("SELECT * FROM transaction_")
-    data = cursor.fetchall()
-    return render_template("transaction.html", records=data)
-
-@app.route('/user')
-def user():
-    cursor.execute("SELECT * FROM user")
-    data = cursor.fetchall()
-    return render_template("user.html", records=data)
-
-# CREATE
-@app.route('/add_user', methods=['POST'])
-def add_user():
-    user_id = request.form['user_id']
-    name = request.form['name']
-    email = request.form['email']
-    phone = request.form['phone']
-=======
 @app.route('/card')
 def Display_Card():
     cursor.execute("SELECT * FROM card")
@@ -165,49 +115,25 @@ def Display_Card():
 def add_card():
 
     if request.method == 'POST':
->>>>>>> 5425bdd466abb6653976fcfbfe443e03648138a1
 
-    cursor.execute(
-        "INSERT INTO user (user_id,name, email, phone) VALUES (%s, %s, %s)",
-        (name, email, phone)
-    )
-    db.commit()
-    return redirect(url_for('user'))
-
-<<<<<<< HEAD
-# DELETE
-@app.route('/delete_user/<int:id>')
-def delete_user(id):
-    cursor.execute("DELETE FROM user WHERE userid=%s", (id,))
-    db.commit()
-    return redirect(url_for('user'))
-=======
         values = (
             request.form['user_id'],
             request.form['card_number'],
             request.form['card_type'],
             request.form['expiry_date']
         )
->>>>>>> 5425bdd466abb6653976fcfbfe443e03648138a1
 
-# UPDATE
-@app.route('/update_user/<int:id>', methods=['POST'])
-def update_user(id):
-    name = request.form['name']
-    email = request.form['email']
-    phone = request.form['phone']
+        cursor.execute(
+            "INSERT INTO card (user_id, card_number, card_type, expiry_date) VALUES (%s, %s, %s, %s)",
+            values
+        )
 
-    cursor.execute("""
-        UPDATE user
-        SET name=%s, email=%s, phone=%s
-        WHERE userid=%s
-    """, (name, email, phone, id))
+        dbconnection.commit()
 
-    db.commit()
-    return redirect(url_for('user'))
+        return redirect(url_for('Display_Card'))
 
-<<<<<<< HEAD
-=======
+    return render_template('add_card.html')
+
 @app.route('/edit/<int:card_id>', methods=['GET', 'POST'])
 def edit_card(card_id):
 
@@ -245,6 +171,17 @@ def delete_card(card_id):
 
     return redirect(url_for('Display_Card'))
 
->>>>>>> 5425bdd466abb6653976fcfbfe443e03648138a1
+@app.route('/user')
+def user():
+    cursor.execute("SELECT * FROM user")
+    data = cursor.fetchall()
+    return render_template("user.html", records=data)
+
+
+
+@app.route('/merchant')
+def merchant():
+    return render_template("merchant.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
